54b49d53645f0b16e76807b22ddd5360fafdad23
cwainright-nps <charles_wainright@nps.gov>
2023-01-09 08:21:52 -0500

    refactor `real_locations` to `real` and `example_locations` to `example` to make template more generalizable. updated success message so user knows where buildRealLocations() output is saved

--- scripts/buildRealLocations.R
+++ scripts/buildRealLocations.R
@@ -1,7 +1,7 @@
-# build example_locations
+# build example
 # a module for `scripts/fish_data_view.R`
 
-buildRealLocations <- function(){
+buildRealLocations <- function(database, connection){
     tryCatch(
         expr = {
             #----- load external libraries
@@ -13,7 +13,7 @@ buildRealLocations <- function(){
             #----- load project functions
             source("scripts/getQueryResults.R") # equivalent to python "from x import function"
             # load example data
-            example_locations <- readxl::read_excel("data/NCRN_BSS_EDD_20230105_1300.xlsx", sheet = "Locations") # https://doimspp.sharepoint.com/:x:/r/sites/NCRNDataManagement/Shared%20Documents/General/Standards/Data-Standards/EQuIS-WQX-EDD/NCRN_BSS_EDD_20230105_1300.xlsx?d=w8c283fde9cbd4af480945c8c8bd94ff6&csf=1&web=1&e=7Y9W1M
+            example <- readxl::read_excel("data/NCRN_BSS_EDD_20230105_1300.xlsx", sheet = "Locations") # https://doimspp.sharepoint.com/:x:/r/sites/NCRNDataManagement/Shared%20Documents/General/Standards/Data-Standards/EQuIS-WQX-EDD/NCRN_BSS_EDD_20230105_1300.xlsx?d=w8c283fde9cbd4af480945c8c8bd94ff6&csf=1&web=1&e=7Y9W1M
             
             # Query db
             db_objs <- RODBC::sqlTables(con) # test db connection
@@ -35,70 +35,70 @@ buildRealLocations <- function(){
             
             # RODBC::odbcCloseAll() # close db connection
             
-            #----- re-build `example_locations` from `results_list`
+            #----- re-build `example` from `results_list`
             
             # starting point: copy the example dataframe but without data
-            real_locations <- tibble::tibble(data.frame(matrix(ncol = ncol(example_locations), nrow = nrow(results_list$tbl_Locations)))) # empty dataframe
-            colnames(real_locations) <- colnames(example_locations) # name columns to match example
+            real <- tibble::tibble(data.frame(matrix(ncol = ncol(example), nrow = nrow(results_list$tbl_Locations)))) # empty dataframe
+            colnames(real) <- colnames(example) # name columns to match example
             
-            real_locations[1] <- "NCRN"
-            real_locations[2] <- results_list$tbl_Locations$Unit_Code
-            real_locations[3] <- results_list$tbl_Locations$Location_ID
-            real_locations[4] <- results_list$tbl_Locations$Loc_Name
-            real_locations[5] <- "Creek"
-            real_locations[6] <- results_list$tbl_Locations$Dec_Degrees_North
-            real_locations[7] <- results_list$tbl_Locations$Dex_Degrees_East
-            real_locations[8] <- "GPS-Unspecified"
-            real_locations[9] <- toupper(results_list$tbl_Locations$Datum)
-            real_locations[10] <- NA
-            real_locations[11] <- NA
-            real_locations[12] <- NA
-            real_locations[13] <- NA
-            real_locations[14] <- NA
-            real_locations[15] <- NA
-            real_locations[16] <- NA
-            real_locations[17] <- results_list$tbl_Locations$HUC
-            real_locations[18] <- sprintf("%.0f", results_list$tbl_Locations$Reach_Code24) # get rid of sci notation
-            real_locations[19] <- NA
-            real_locations[20] <- NA
-            for(i in 1:nrow(real_locations)){ # loop runs once per row in `real_locations`
+            real[1] <- "NCRN"
+            real[2] <- results_list$tbl_Locations$Unit_Code
+            real[3] <- results_list$tbl_Locations$Location_ID
+            real[4] <- results_list$tbl_Locations$Loc_Name
+            real[5] <- "Creek"
+            real[6] <- results_list$tbl_Locations$Dec_Degrees_North
+            real[7] <- results_list$tbl_Locations$Dex_Degrees_East
+            real[8] <- "GPS-Unspecified"
+            real[9] <- toupper(results_list$tbl_Locations$Datum)
+            real[10] <- NA
+            real[11] <- NA
+            real[12] <- NA
+            real[13] <- NA
+            real[14] <- NA
+            real[15] <- NA
+            real[16] <- NA
+            real[17] <- results_list$tbl_Locations$HUC
+            real[18] <- sprintf("%.0f", results_list$tbl_Locations$Reach_Code24) # get rid of sci notation
+            real[19] <- NA
+            real[20] <- NA
+            for(i in 1:nrow(real)){ # loop runs once per row in `real`
                 if(results_list$tbl_Locations$Elevation[i] == 0){ # change zeroes to NA
-                    real_locations[i,21] <- NA # b/c all NCRN sites are above sea level
+                    real[i,21] <- NA # b/c all NCRN sites are above sea level
                 } else { # otherwise just keep the listed elevation
-                    real_locations[i,21] <- results_list$tbl_Locations$Elevation[i]
+                    real[i,21] <- results_list$tbl_Locations$Elevation[i]
                 }
             }
-            real_locations[22] <- "ft"
-            real_locations[23] <- NA
-            real_locations[24] <- NA
-            real_locations[25] <- NA
-            real_locations[26] <- NA
-            real_locations[27] <- "US"
-            real_locations[28] <- results_list$tbl_Locations$State
-            real_locations[29] <- results_list$tbl_Locations$County
-            real_locations[30] <- sprintf("%.3f", results_list$tbl_Locations$Catchment_Area)
-            real_locations[31] <- "acre" # design view db.tbl_Locations
-            real_locations[32] <- NA
-            real_locations[33] <- NA
-            real_locations[34] <- NA
-            real_locations[35] <- NA
-            real_locations[36] <- NA
-            real_locations[37] <- NA
-            real_locations[38] <- NA
-            real_locations[39] <- NA
-            real_locations[40] <- NA
-            real_locations[41] <- NA
-            real_locations[42] <- NA
-            assign("real_locations", real_locations, envir = globalenv())
-            # assign("example_locations", example_locations, envir = globalenv())
+            real[22] <- "ft"
+            real[23] <- NA
+            real[24] <- NA
+            real[25] <- NA
+            real[26] <- NA
+            real[27] <- "US"
+            real[28] <- results_list$tbl_Locations$State
+            real[29] <- results_list$tbl_Locations$County
+            real[30] <- sprintf("%.3f", results_list$tbl_Locations$Catchment_Area)
+            real[31] <- "acre" # design view db.tbl_Locations
+            real[32] <- NA
+            real[33] <- NA
+            real[34] <- NA
+            real[35] <- NA
+            real[36] <- NA
+            real[37] <- NA
+            real[38] <- NA
+            real[39] <- NA
+            real[40] <- NA
+            real[41] <- NA
+            real[42] <- NA
+            assign("real", real, envir = globalenv())
+            # assign("example", example, envir = globalenv())
             
             # error-checking:
-            check_df <- tibble::tibble(data.frame(matrix(ncol=3, nrow=ncol(real_locations))))
-            colnames(check_df) <- c("real_locations", "example_locations", "result")
-            check_df$real_locations <- colnames(real_locations)
-            check_df$example_locations <- colnames(example_locations)
+            check_df <- tibble::tibble(data.frame(matrix(ncol=3, nrow=ncol(real))))
+            colnames(check_df) <- c("real", "example", "result")
+            check_df$real <- colnames(real)
+            check_df$example <- colnames(example)
             for(i in 1:nrow(check_df)){
-                if(check_df$real_locations[i] == check_df$example_locations[i]){
+                if(check_df$real[i] == check_df$example[i]){
                     check_df$result[i] <- "MATCH"
                 } else {
                     check_df$result[i] <- "MISMATCH"
@@ -107,10 +107,10 @@ buildRealLocations <- function(){
             
             message(
                 if(length(check_df$result == "MATCH") == nrow(check_df)){
-                    "buildRealLocations() executed successfully; `real_locations` colnames match `example_locations`"
+                    "`buildRealLocations()` executed successfully; `real` colnames match `example`"
                 } else {
                     for(i in 1:length(check_df$result != "MATCH")){
-                        cat(paste(paste0("`real_locations.", check_df$real_locations[i], "`"), paste0(" DID NOT MATCH `example_locations.", check_df$example_locations[i][i], "`"), "\n", sep = ""))
+                        cat(paste(paste0("`real.", check_df$real[i], "`"), paste0(" DID NOT MATCH `example.", check_df$example[i][i], "`"), "\n", sep = ""))
                     }
                 }
             )
