f6d117f1157092467f9582189531de33837d9123
cwainright-nps <charles_wainright@nps.gov>
2023-01-09 16:44:12 -0500

    initial commit

---
+++ scripts/buildEDD.R
@@ -0,0 +1,8 @@
+# build electronic data deliverable (EDD)
+# EDD is a data format https://doimspp.sharepoint.com/sites/NCRNBiologicalStreamSampling/Shared%20Documents/Forms/AllItems.aspx?csf=1&web=1&e=oKeaBg&cid=46a9b284%2D304d%2D4b63%2D9d30%2D64b97cdd0314&RootFolder=%2Fsites%2FNCRNBiologicalStreamSampling%2FShared%20Documents%2FGeneral%2FAnnual%2DData%2DPackages%2F2022%2FExamples&FolderCTID=0x0120002FA57F4A20C6EC439BFEBEF4FA9D88E0
+# database is from: https://doimspp.sharepoint.com/:u:/r/sites/NCRNBiologicalStreamSampling/Shared%20Documents/General/Annual-Data-Packages/2022/NCRN_MBSS/NCRN_MBSS_be_2022.mdb?csf=1&web=1&e=jjeJIg
+# a module for `scripts/fish_data_view.R`
+
+# source("scripts/buildRealLocations.R")
+# source("scripts/buildRealActivities.R")
+# source("scripts/buildRealResults.R")
