53fe2960f2e73a96952093f3cd64716e0f6bf64c
cwainright-nps <charles_wainright@nps.gov>
2023-01-10 11:24:09 -0500

    added comments to output fields, added logic to parse values from NA and nonsense

--- scripts/buildRealActivities.R
+++ scripts/buildRealActivities.R
@@ -24,7 +24,8 @@ buildRealActivities <- function(connection){
                     "tbl_Fish_Events",
                     "tbl_Locations",
                     "tbl_Meta_Events",
-                    "tlu_Collection_Procedures_Gear_Config"
+                    "tlu_Collection_Procedures_Gear_Config",
+                    "tbl_Electro_Fish_Details"
                     )
                     ) %>%
                 select(TABLE_NAME)
@@ -40,10 +41,12 @@ buildRealActivities <- function(connection){
             
             # make a flat dataframe from `results_list`
             df <- results_list$tbl_Fish_Events %>% select(-c(Fish_Move, Bottom_Visible, Water_Quality_2, Fish_Biomass_1, Fish_Biomass_2))
-            df <- dplyr::left_join(df, results_list$tbl_Events %>% select(Event_ID, Protocol_ID, Start_Date, Start_Time, Location_ID, Comments, Event_Group_ID), by = "Event_ID")
+            df <- dplyr::left_join(df, results_list$tbl_Events %>% select(Event_ID, Protocol_ID, Start_Date, Start_Time, Location_ID, Comments, Event_Site_ID, Event_Group_ID), by = "Event_ID")
             df <- dplyr::left_join(df, results_list$tbl_Protocol %>% select(Protocol_ID, Protocol_Name, Protocol_Version, Version_Date), by = "Protocol_ID")
             df <- dplyr::left_join(df, results_list$tbl_Meta_Events %>% select(Event_ID, Entered_by), by = "Event_ID")
             df <- dplyr::left_join(df, results_list$tbl_Locations %>% select(Location_ID, Loc_Name, NCRN_Site_ID), by = "Location_ID")
+            df2 <- results_list$tbl_Electro_Fish_Details %>% dplyr::distinct(Fish_Event_ID, .keep_all = TRUE)
+            df <- dplyr::left_join(df, df2 %>% select(Fish_Event_ID, Pass_1_Start, Pass_1_End, Pass_2_Start, Pass_2_End), by = "Fish_Event_ID")
             
             # RODBC::odbcCloseAll() # close db connection
             
@@ -53,47 +56,62 @@ buildRealActivities <- function(connection){
             real <- tibble::tibble(data.frame(matrix(ncol = ncol(example), nrow = nrow(results_list$tbl_Fish_Events)))) # empty dataframe
             colnames(real) <- colnames(example) # name columns to match example
             
-            real[1] <- "NCRN"
-            real[2] <- df$Protocol_Name # query: SELECT e.Protocol_ID FROM tbl_Events e LEFT JOIN SELECT(Protocol_ID, Protocol_Name FROM tbl_Protocol p) ON e.Protocol_ID = p.Protocol_ID
-            real[3] <- df$Event_Site_ID
-            real[4] <- df$Fish_Event_ID
-            real[5] <- "Field Msr/Obs" # Activity Type; choices are: 1) 'Field Msr/Obs' and 2) 'Sample-Routine'
-            real[6] <- "Water" # choices are "Water", "Air", and "Other" in `example`
-            real[7] <- NA # NA in `example`
-            real[9] <- df$Start_Date
-            real[10] <- df$Start_Time
-            real[11] <- "Eastern Time - Washington, DC"
-            real[12] <- NA # Activity end date
-            real[13] <- NA # activity end time
-            real[14] <- NA # Activity_End_Time_Zone
-            real[15] <- NA # relative depth
-            real[16] <- NA # depth
-            real[17] <- NA # depth unit
-            real[18] <- NA # activity upper depth
-            real[19] <- NA # activity lower depth
-            real[20] <- NA # activity depth reference
-            real[21] <- df$Loc_Name # additional location info
-            real[22] <- NA # activity sampler; the person who did the sampling?
-            real[23] <- df$Entered_by # Activity_Recorder
-            real[24] <- df$NCRN_Site_ID # Custody_ID
-            real[25] <- "NCRN" # Activity_Conducting_Organization
-            real[26] <- NA # Station_Visit_Comment
-            real[27] <- df$Comments # Activity_Comment
-            real[28] <- paste0(df$Protocol_Name, "; version", df$Protocol_Version, "; protocol date ", df$Version_Date) # "Preparation_Method_ID"
-            real[29] <- "Smith Root LR-24" # "Collection_Equipment_Name" 
-            real[30] <- "Smith Root LR-24" # "Collection_Equipment_Description" 
-            real[31] <- subset(results_list$tlu_Collection_Procedures_Gear_Config, `Field Gear Category` == "Smith Root LR-24")$`Field Procedure ID` # "Gear_Deployment"
-            real[32] <- NA # container type
-            real[33] <- NA # container color
-            real[34] <- NA # container size
-            real[35] <- NA # container size unit
-            real[36] <- paste0(df$Protocol_Name, "; version", df$Protocol_Version, "; protocol date ", df$Version_Date) # "Preparation_Method_ID"
+            real[1] <- "NCRN" # "#Org_Code" 
+            real[2] <- df$Protocol_Name # "Project_ID"
+            real[3] <- df$Event_Site_ID # "Location_ID" 
+            real[4] <- df$Fish_Event_ID # "Activity_ID"
+            real[5] <- "Field Msr/Obs" # "Activity_Type"; choices are: 1) 'Field Msr/Obs' and 2) 'Sample-Routine'
+            real[6] <- "Water" # "Medium"  choices are "Water", "Air", and "Other" in `example`
+            real[7] <- NA # "Medium_Subdivision"
+            real[8] <- "Stream Fish" # "Assemblage_Sampled_Name"
+            real[9] <- df$Start_Date # "Activity_Start_Date"
+            real[10] <- format(df$Start_Time, "%H:%M") # "Activity_Start_Time" 
+            real[11] <- "Eastern Time - Washington, DC" # "Activity_Start_Time_Zone" 
+            real[12] <- NA # "Activity_End_Date" 
+            real[13] <- NA # "Activity_End_Time"
+            real[14] <- NA # "Activity_End_Time_Zone"  
+            real[15] <- NA # "Activity_Relative_Depth" 
+            real[16] <- NA # "Activity_Depth"
+            real[17] <- NA # "Activity_Depth_Unit"
+            real[18] <- NA # "Activity_Upper_Depth"
+            real[19] <- NA # "Activity_Lower_Depth"
+            real[20] <- NA # "Activity_Depth_Reference"
+            real[21] <- df$Loc_Name # "Additional_Location_Info"
+            real[22] <- NA # "Activity_Sampler"; the person who did the sampling?
+            real[23] <- df$Entered_by # "Activity_Recorder"
+            # regex to extract strings matching pattern:
+            # String starts with (^) any character (*)
+            # followed by one or more (+) numbers ([0-9])
+            # followed by a dash (-)
+            # followed by one or more (+) numbers ([0-9])
+            # followed by a period (.)
+            # followed by one or more (+) numbers ([0-9])
+            for (i in 1:nrow(real)){ # for each row
+                ifelse(stringr::str_detect(real$Activity_Recorder[i], "Event") == TRUE, # 75 m is the prescribed e-fishing reach distance; 
+                       real[i,23] <- stringr::str_extract(real[i,23], "^*([0-9])+-([0-9])+.([0-9])+"),
+                       real[i,23] <- real[i,23])# units are meters
+            }
+            real[24] <- df$NCRN_Site_ID # "Custody_ID" 
+            real[25] <- "NCRN" # "Activity_Conducting_Organization" 
+            real[26] <- NA # "Station_Visit_Comment" 
+            real[27] <- df$Comments # "Activity_Comment
+            real[28] <- paste0(df$Protocol_Name, "; version ", df$Protocol_Version, "; protocol date ", df$Version_Date) # "Collection_Method_ID" 
+            real[29] <- NA # Possibly a Smith Root LR-24 but not known; "Collection_Equipment_Name" 
+            real[30] <- NA # Possibly a Smith Root LR-24 but not known; # "Collection_Equipment_Description" 
+            real[31] <- NA # subset(results_list$tlu_Collection_Procedures_Gear_Config, `Field Gear Category` == "Smith Root LR-24")$`Field Procedure ID` # "Gear_Deployment"
+            real[32] <- NA # "Container_Type"
+            real[33] <- NA # "Container_Color"
+            real[34] <- NA # "Container_Size"
+            real[35] <- NA # "Container_Size_Unit"
+            real[36] <- paste0(df$Protocol_Name, "; version ", df$Protocol_Version, "; protocol date ", df$Version_Date) # "Preparation_Method_ID"
             real[37] <- "10% buffered formalin solution (later transferable to 70% EtOH solution)" # "Chemical_Preservative" # pdf pg 124 (135) https://doimspp.sharepoint.com/:b:/r/sites/NCRNBiologicalStreamSampling/Shared%20Documents/General/Operational%20Reviews/NCRN_Biological_Stream_Survey_Protocol_Ver_2.0_NRR.pdf?csf=1&web=1&e=u0kGN9
             real[38] <- NA # "Thermal_Preservative". Fish are preserved via chemicals, not wet ice
             real[39] <- NA # "Transport_Storage_Description" 
             real[40] <- df$Event_Group_ID # "Activity_Group_ID"
             real[41] <- df$NCRN_Site_ID # "Activity_Group_Name" 
-            real[42] <- paste0("Activities for: ", df$NCRN_Site_ID) # "Activity_Group_Type"  
+            real[42] <- ifelse(is.na(real[41])==TRUE,
+                               paste0("Activities for: ", df$NCRN_Site_ID),
+                               NA)# "Activity_Group_Type"  
             real[43] <- NA # we don't record stop time for fish events e-fishing, so duration is unknown
             real[44] <- NA # we don't record stop time for fish events e-fishing, so duration unit is unknown
             real[45] <- "Two-pass backpack electrofishing" # Sampling_Component_Name
@@ -107,21 +125,30 @@ buildRealActivities <- function(connection){
                         real[i,48] <- NA #assign NA when "Reach_Length" is blank
                     }
                 }
-            real[49] <- NA # Reach_Width
-            real[50] <- NA # Reach_Width_Unit
-            real[51] <- 2 # [electrofishing] pass count
-            real[52] <- NA # net type
-            real[53] <- NA # net surface area
-            real[54] <- NA # net surface area unit
-            real[55] <- NA # net mesh size
-            real[56] <- NA # net mesh size unit
-            real[57] <- NA # boat speed
-            real[58] <- NA # boat speed unit
-            real[59] <- NA # Current speed
-            real[60] <- NA # current speed unit
-            real[61] <- NA # toxicity test type
-            real[62] <- NA # effort # we don't record stop time for fish events, so effort is unknown
-            real[63] <- NA # effort unit
+            real[49] <- NA # "Reach_Width"
+            real[50] <- NA # "Reach_Width_Unit" 
+            # [electrofishing] "Pass_Count"
+            # only assign a "Pass_Count" value if we know the reach length
+            # because this gives us higher confidence that the record used the 2-pass e-fishing protocol
+            for (i in 1:nrow(real)){ # for each row
+                if(!is.na(real[i,47])){ # 75 m is the prescribed e-fishing reach distance; 
+                    real[i,51] <- 2 # units are meters
+                } else {# otherwise
+                    real[i,51] <- NA #assign NA when "Reach_Length" is blank
+                }
+            }
+            real[52] <- NA # "Net_Type"
+            real[53] <- NA # "Net_Surface_Area"
+            real[54] <- NA # "Net_Surface_Area_Unit"
+            real[55] <- NA # "Net_Mesh_Size"
+            real[56] <- NA # "Net_Mesh_Size_Unit"  
+            real[57] <- NA # "Boat_Speed"
+            real[58] <- NA # "Boat_Speed_Unit"
+            real[59] <- NA # "Current_Speed"
+            real[60] <- NA # "Current_Speed_Unit"
+            real[61] <- NA # "Toxicity_Test_Type"
+            real[62] <- NA # "Effort" # we don't record stop time for fish events, so effort is unknown
+            real[63] <- NA # "Effort_Unit"
             
             assign("real_activities", real, envir = globalenv())
             # assign("example", example, envir = globalenv())
