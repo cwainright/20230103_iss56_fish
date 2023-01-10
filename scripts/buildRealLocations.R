e626ebc0faf2e35d516fd7501f1c5ca30085e51a
cwainright-nps <charles_wainright@nps.gov>
2023-01-10 12:28:27 -0500

    notated shared fields, added logic to define when `$Elevation_Unit` should be NA

--- scripts/buildRealLocations.R
+++ scripts/buildRealLocations.R
@@ -41,26 +41,27 @@ buildRealLocations <- function(connection){
             real <- tibble::tibble(data.frame(matrix(ncol = ncol(example), nrow = nrow(results_list$tbl_Locations)))) # empty dataframe
             colnames(real) <- colnames(example) # name columns to match example
             
-            real[1] <- "NCRN"
-            real[2] <- results_list$tbl_Locations$Unit_Code
-            real[3] <- results_list$tbl_Locations$Location_ID
-            real[4] <- results_list$tbl_Locations$Loc_Name
-            real[5] <- "Creek"
-            real[6] <- results_list$tbl_Locations$Dec_Degrees_North
-            real[7] <- results_list$tbl_Locations$Dex_Degrees_East
-            real[8] <- "GPS-Unspecified"
-            real[9] <- toupper(results_list$tbl_Locations$Datum)
-            real[10] <- NA
-            real[11] <- NA
-            real[12] <- NA
-            real[13] <- NA
-            real[14] <- NA
-            real[15] <- NA
-            real[16] <- NA
-            real[17] <- results_list$tbl_Locations$HUC
-            real[18] <- sprintf("%.0f", results_list$tbl_Locations$Reach_Code24) # get rid of sci notation
-            real[19] <- NA
-            real[20] <- NA
+            real[1] <- "NCRN" # "#Org_Code"
+            real[2] <- results_list$tbl_Locations$Unit_Code # "Park_Code" 
+            real[3] <- results_list$tbl_Locations$Location_ID # "Location_ID" shared field with `real_activities.Location_ID`
+            real[4] <- results_list$tbl_Locations$Loc_Name # "Location_Name"
+            real[5] <- "Creek" # "Location_Type"
+            real[6] <- results_list$tbl_Locations$Dec_Degrees_North # "Latitude"
+            real[7] <- results_list$tbl_Locations$Dex_Degrees_East # "Longitude"
+            real[8] <- "GPS-Unspecified" # "Lat_Lon_Method"
+            real[9] <- toupper(results_list$tbl_Locations$Datum) # "Lat_Lon_Datum"
+            real[10] <- NA # "Source_Map_Scale_Numeric" 
+            real[11] <- NA # "Lat_Lon_Accuracy"
+            real[12] <- NA # "Lat_Lon_Accuracy_Unit"
+            real[13] <- NA # "Location_Description"
+            real[14] <- NA # "Travel_Directions"
+            real[15] <- NA # "Location_Purpose"
+            real[16] <- NA # "Establishment_Date" 
+            real[17] <- results_list$tbl_Locations$HUC # "HUC8_Code"
+            real[18] <- sprintf("%.0f", results_list$tbl_Locations$Reach_Code24) # "HUC12_Code"; remove sci notation
+            real[19] <- NA # "Alternate_Location_ID"
+            real[20] <- NA # "Alternate_Location_ID_Context"
+            # "Elevation" 
             for(i in 1:nrow(real)){ # loop runs once per row in `real`
                 if(results_list$tbl_Locations$Elevation[i] == 0){ # change zeroes to NA
                     real[i,21] <- NA # b/c all NCRN sites are above sea level
@@ -68,27 +69,34 @@ buildRealLocations <- function(connection){
                     real[i,21] <- results_list$tbl_Locations$Elevation[i]
                 }
             }
-            real[22] <- "ft"
-            real[23] <- NA
-            real[24] <- NA
-            real[25] <- NA
-            real[26] <- NA
-            real[27] <- "US"
-            real[28] <- results_list$tbl_Locations$State
-            real[29] <- results_list$tbl_Locations$County
-            real[30] <- sprintf("%.3f", results_list$tbl_Locations$Catchment_Area)
-            real[31] <- "acre" # design view db.tbl_Locations
-            real[32] <- NA
-            real[33] <- NA
-            real[34] <- NA
-            real[35] <- NA
-            real[36] <- NA
-            real[37] <- NA
-            real[38] <- NA
-            real[39] <- NA
-            real[40] <- NA
-            real[41] <- NA
-            real[42] <- NA
+            # "Elevation_Unit" 
+            for(i in 1:nrow(real)){ # loop runs once per row in `real`
+                if(is.na(real[i,21]) == FALSE){ # change zeroes to NA
+                    real[i, 22] <- "ft" # b/c all NCRN sites are above sea level
+                } else { # otherwise just keep the listed elevation
+                    real[i,22] <- NA
+                }
+            }
+            real[23] <- NA # "Elevation_Method" 
+            real[24] <- NA # "Elevation_Datum"
+            real[25] <- NA # "Elevation_Accuracy"
+            real[26] <- NA # "Elevation_Accuracy_Unit"
+            real[27] <- "US" # "Country_Code"
+            real[28] <- results_list$tbl_Locations$State # "State_Code"
+            real[29] <- results_list$tbl_Locations$County # "County_Name"
+            real[30] <- sprintf("%.3f", results_list$tbl_Locations$Catchment_Area) # "Drainage_Area"
+            real[31] <- "acre" # "Drainage_Area_Unit"; design view db.tbl_Locations
+            real[32] <- NA # "Contributing_Area"
+            real[33] <- NA # "Contributing_Area_Unit"
+            real[34] <- NA # "Tribal_Land_Indicator"
+            real[35] <- NA # "Tribal_Land_Name"
+            real[36] <- NA # "Well_ID"
+            real[37] <- NA # "Well_Type"
+            real[38] <- NA # "Aquifer_Name"
+            real[39] <- NA # "Formation_Type"
+            real[40] <- NA # "Well_Hole_Depth"
+            real[41] <- NA # "Well_Hole_Depth_Unit"
+            real[42] <- NA # "Well_Status"
             assign("real_locations", real, envir = globalenv())
             # assign("example", example, envir = globalenv())
             
