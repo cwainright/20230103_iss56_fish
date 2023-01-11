ff8957fc60eaba98f17577ffe3fac5d0a1593ce7
cwainright-nps <charles_wainright@nps.gov>
2023-01-11 17:00:40 -0500

    started py workflow

--- scripts/py/main.py
+++ scripts/py/main.py
@@ -11,28 +11,17 @@
   # colnames
   # column order
 
-import pandas as pd
-import numpy as np
-import pyodbc
+# import pandas as pd
+# import numpy as np
+# import pyodbc
 
-# look at example data
-example_data = pd.read_csv("data/data_fish_MBSS.csv")
-example_data.describe()
-for col in example_data.columns:
-    print(col)
-example_data.groupby(['TAXAID']).count()
+from importlib import reload
+import getQueryResults
+reload(getQueryResults)
 
-# look at database
-driver = "{Microsoft Access Driver (*.mdb, *.accdb)}"
 dbq = r"C:\Users\cwainright\OneDrive - DOI\Documents - NPS-NCRN-Biological Stream Sampling\General\Annual-Data-Packages\2022\NCRN_MBSS\NCRN_MBSS_be_2022.mdb"
-con = pyodbc.connect(f'Driver={driver};DBQ={dbq};') # establish db connection
-cursor = con.cursor() # establish db cursor to interact with db
-for i in cursor.tables(tableType='TABLE'): # test db connection by extracting table names
-    print(i.table_name)
+getQueryResults.getQueryResults(dbq = dbq)
 
-query = 'SELECT TOP 2 * FROM tlu_Macroinverts'
-# query = "SELECT 'sqlserver' dbms,t.TABLE_CATALOG,t.TABLE_SCHEMA,t.TABLE_NAME,c.COLUMN_NAME,c.ORDINAL_POSITION,c.DATA_TYPE,c.CHARACTER_MAXIMUM_LENGTH,n.CONSTRAINT_TYPE,k2.TABLE_SCHEMA,k2.TABLE_NAME,k2.COLUMN_NAME FROM INFORMATION_SCHEMA.TABLES t LEFT JOIN INFORMATION_SCHEMA.COLUMNS c ON t.TABLE_CATALOG=c.TABLE_CATALOG AND t.TABLE_SCHEMA=c.TABLE_SCHEMA AND t.TABLE_NAME=c.TABLE_NAME LEFT JOIN(INFORMATION_SCHEMA.KEY_COLUMN_USAGE k JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS n ON k.CONSTRAINT_CATALOG=n.CONSTRAINT_CATALOG AND k.CONSTRAINT_SCHEMA=n.CONSTRAINT_SCHEMA AND k.CONSTRAINT_NAME=n.CONSTRAINT_NAME LEFT JOIN INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS r ON k.CONSTRAINT_CATALOG=r.CONSTRAINT_CATALOG AND k.CONSTRAINT_SCHEMA=r.CONSTRAINT_SCHEMA AND k.CONSTRAINT_NAME=r.CONSTRAINT_NAME)ON c.TABLE_CATALOG=k.TABLE_CATALOG AND c.TABLE_SCHEMA=k.TABLE_SCHEMA AND c.TABLE_NAME=k.TABLE_NAME AND c.COLUMN_NAME=k.COLUMN_NAME LEFT JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE k2 ON k.ORDINAL_POSITION=k2.ORDINAL_POSITION AND r.UNIQUE_CONSTRAINT_CATALOG=k2.CONSTRAINT_CATALOG AND r.UNIQUE_CONSTRAINT_SCHEMA=k2.CONSTRAINT_SCHEMA AND r.UNIQUE_CONSTRAINT_NAME=k2.CONSTRAINT_NAME WHERE t.TABLE_TYPE='BASE TABLE';"
-df = pd.read_sql(query,con) # use pandas to extract query results as dataframe
+# cursor.close()
+# con.close() # close db connection
 
-cursor.close()
-con.close() # close db connection
