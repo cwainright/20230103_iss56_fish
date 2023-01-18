fbf3940ad9313989f8ff89a1ea4e415118451e59
cwainright-nps <charles_wainright@nps.gov>
2023-01-18 08:58:53 -0500

    removed hard-coded table names from `getQueryResults.R` and changed table names to a parameter of `getQueryResults()` so make the function becomes a generic db query function that a user can pass table names to

--- scripts/getQueryResults.R
+++ scripts/getQueryResults.R
@@ -1,38 +1,9 @@
 # query a database and return results to a list
 # a module for `scripts/fish_data_view.R`
 
-getQueryResults <- function(connection){ # function with one argument: 1.`connection` an active odbc connection
+getQueryResults <- function(qry_list, connection){ # function with one argument: 1.`connection` an active odbc connection
     tryCatch(
         expr = {
-            
-            # Query db
-            db_objs <- RODBC::sqlTables(con) # test db connection
-            tbl_names <- db_objs %>% # choose which tables you want to query
-                subset(TABLE_NAME %in% c(
-                    "tbl_Events",
-                    "tbl_Protocol",
-                    "tbl_Fish_Events",
-                    "tbl_Locations",
-                    "tbl_Meta_Events",
-                    "tbl_Fish_Data",
-                    "tbl_GameFish",
-                    "tlu_Fish",
-                    "tlu_Collection_Procedures_Gear_Config",
-                    "tbl_Electro_Fish_Details",
-                    "tlu_Basin_Code",
-                    "tbl_Photos",
-                    "tlu_Park_Code"
-                )
-                ) %>%
-                select(TABLE_NAME)
-            
-            # make list of queries so we can extract a few rows from each table
-            qry_list <- vector(mode="list", length=nrow(tbl_names))
-            names(qry_list) <- tbl_names$TABLE_NAME
-            for (i in 1:length(qry_list)){
-                qry_list[[i]] <- paste("SELECT * FROM", names(qry_list)[i])
-            }
-            
             results_list <- vector(mode="list", length=length(qry_list)) # instantiate empty list to hold query results
             names(results_list) <- names(qry_list) # name elements in `results_list` to match `qry_list` element names
             for(i in 1:length(qry_list)){ # loop runs once per query
